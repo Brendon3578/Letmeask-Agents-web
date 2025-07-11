975f6ff72fc65a9cada9ea72e19635bc9e2fd663
Brendon Gomes <brendongomes40@gmail.com>
2025-07-10 21:58:50 -0300

    feat: add biome configuration file and remove all unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
 
-createRoot(document.getElementById('root')!).render(
+// biome-ignore lint/style/noNonNullAssertion: mandatory by react
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
